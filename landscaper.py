93e3b2b8c4594e7e50f9787e647c5d927b5a3b09
pentiumKing <ramahawai87@gmail.com>
2023-10-11 17:51:30 -0400

    initial commit

---
+++ landscaper.py
@@ -0,0 +1,16 @@
+money = 0
+tools = []
+win_amount = 1000
+
+# Define actions
+actions = [
+    {"name": "Cut grass with teeth", "earnings": 1},
+    {"name": "Buy rusty scissors", "cost": 5, "earnings": 5},
+    {"name": "Use rusty scissors", "earnings": 5},
+    {"name": "Buy old-timey push lawnmower", "cost": 25, "earnings": 50},
+    {"name": "Use old-timey push lawnmower", "earnings": 50},
+    {"name": "Buy fancy battery-powered lawnmower", "cost": 250, "earnings": 100},
+    {"name": "Use fancy battery-powered lawnmower", "earnings": 100},
+    {"name": "Hire a team of starving students", "cost": 500, "earnings": 250},
+    {"name": "Use a team of starving students", "earnings": 250}
+]
